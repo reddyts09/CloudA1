f2921e629bc6379bf02da3b6056be7bf9721d5f6
Tarun <tarunreddy1996@gmail.com>
2019-02-07 23:31:24 -0500

    Update fact.js

--- uwsgi-tut/mysite/static/js/fact.js
+++ uwsgi-tut/mysite/static/js/fact.js
@@ -1,9 +1,19 @@
 function show(){
 var i, no, fact;
 fact=1;
-no=Number(document.getElementById("num").value);
-for(i=1; i<=no; i++)  
-{
+if (isNaN(no)) {
+alert("Enter a Valid Number and not a string");
+document.getElementById("num").value='';
+document.getElementById("answer").value='';
+return false;
+}
+if (no<=-1) {
+alert("Enter a number greater than or equal to 0");
+document.getElementById("num").value='';
+document.getElementById("answer").value='';
+return false;
+}
+for(i=1; i<=no; i++) {
 fact= fact*i;
 }  
 document.getElementById("answer").value= fact;
